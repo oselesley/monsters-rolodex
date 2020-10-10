7805726c4cee93aae833fced50cab62b4ff49168
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-10-10 02:31:08 +0100

    Updates

---
+++ precache-manifest.ff27662c21f7db06ef1a15dd2a914521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f245c7a591d0d133149771a2c21889",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d21ba97e7d4e9d73a8a9",
+    "url": "/monsters-rolodex/static/css/main.56a62790.chunk.css"
+  },
+  {
+    "revision": "9c15f0b0161fce5a1058",
+    "url": "/monsters-rolodex/static/js/2.e03e8bf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e03e8bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21ba97e7d4e9d73a8a9",
+    "url": "/monsters-rolodex/static/js/main.02d4c02f.chunk.js"
+  },
+  {
+    "revision": "028b8efa47b2b07b6849",
+    "url": "/monsters-rolodex/static/js/runtime-main.fce491ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
